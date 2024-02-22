3c14b74424ef06124ef39b09d2a2207c76dd0e4a
saikat <rick711c@gmail.com>
2024-02-23 02:12:30 +0530

    login card sm screen done

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -7,6 +7,8 @@ import {
   CardMedia,
   Grid,
   ThemeProvider,
+  TextField,
+  Button,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import theme from "../../theme/theme";
@@ -14,106 +16,83 @@ import theme from "../../theme/theme";
 export const Login = () => {
   const navigate = useNavigate();
   return (
-    // <Box
-    //   sx={{
-    //     display: "flex",
-    //     border: 2,
-    //     borderColor: "green",
-    //     height: 416,
-    //     width: 604  ,
-    //     justifyContent: 'space-around',
-    //     alignItems: 'center',
-    //     pl:24,
-    //     pr:24,
-    //     paddingTop:16,
-    //     paddingBottom:16,
-
-    //   }}
-    // >
-    //   <Card sx={{ display: "flex", border: 2 }}>
-    //     <Box
-    //      sx={{
-    //         display: "flex",
-    //         border: 2,
-    //         borderColor: "red",
-    //         height: 288,
-    //         width: 255,
-
-    //       }}
-    //     >
-    //       <CardMedia
-    //         component="img"
-    //         // height="256"
-    //         // width="288"
-    //         image={require("../../assets/images/male-doctor-wearing-surgical-gown-svgrepo-com 1.jpg")}
-    //         alt="Paella dish"
-    //       />
-    //     </Box>
-
-    //     <Box
-    //     sx={{
-    //         display: "flex",
-    //         border: 2,
-    //         borderColor: "red",
-    //         height: 288,
-    //         width: 255,
-    //       }}
-    //     >
-    //       <CardContent>
-    //         <Typography variant="body2" color="text.secondary">
-    //           if you like.
-    //         </Typography>
-    //       </CardContent>
-    //     </Box>
-    //   </Card>
-    // </Box>
-
-    <Card sx={{ padding: "16px 24px", border: 2, height: 416, width: 604 }}>
+    <ThemeProvider theme={theme}>
       <Box
         sx={{
           display: "flex",
-          alignItems: "center",
           justifyContent: "center",
-          height: "100%",
+          alignItems: "center",
+          height: "100vh", // Set height to fill the viewport
         }}
       >
-        <Grid container spacing={0} justifyContent="space-between">
-          {/* masked doctor image */}
-          <Grid item xs={12} sm={6}>
-            <Box
-              sx={{
-                display: "flex",
-                border: 2,
-                borderColor: "red",
-                height: 288,
-                width: 255,
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={require("../../assets/images/male-doctor-wearing-surgical-gown-svgrepo-com 1.jpg")}
-                alt="Paella dish"
-              />
-            </Box>
-          </Grid>
+        <Card
+          sx={{
+            padding: "20px",
+            backgroundColor: theme.palette.background.default,
+            height: { xs: "100%", sm: 416 },
+            width: { xs: "100%", sm: 604 },
+            justifyContent: { xs: "center", sm: "space-around" }, // Removed curly braces
+            gap: { xs: 4 },
+            alignItems: "center",
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" }, // "row" instead of "Row"
+            // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)"
+          }}
+        > 
+          <Box
+            sx={{
+              display: "flex",
+              border: 2,
+              borderColor: "red",
+              height: 288,
+              width: 255,
+            }}
+          >
+            <CardMedia
+              component="img"
+              image={require("../../assets/images/male-doctor-wearing-surgical-gown-svgrepo-com 1.jpg")}
+              alt="Paella dish"
+            />
+          </Box>
 
-          <Grid item xs={12} sm={6}>
-            <Box
-              sx={{
-                display: "flex",
-                border: 2,
-                borderColor: "red",
-                height: 288,
-                width: 255,
-              }}
-            >
-              <ThemeProvider theme={theme}>
-                <Typography variant="h2">Let's Get Started</Typography>
-              </ThemeProvider>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column", // Display items in a column
+              justifyContent: "space-evenly",
+              border: 2,
+              borderColor: "red",
+              height: 288,
+              width: 255,
+            }}
+          >
+            {/* Text TextField, Button */}
+            <Box overflow={"auto"}>
+              <Typography variant="h2">Let's Get Started</Typography>
+              <Typography variant="body1">
+                An OTP will be sent to verify the mobile number
+              </Typography>
             </Box>
-          </Grid>
-        </Grid>
+
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              placeholder="Mobile No"
+            />
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: theme.palette.primary.main,
+                color: "white",
+              }}
+              size="large"
+            >
+              Get OTP
+            </Button>
+          </Box>
+        </Card>
       </Box>
-    </Card>
+    </ThemeProvider>
   );
 };
--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,13 +1,49 @@
 // theme/theme.js
 
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
     h2: {
-      fontSize: '22px', // Set the desired font size
+      fontSize: "22px",
+      fontWeight: "600", // Semi-bold
+    },
+
+    h3: {
+      fontSize: "20px",
+      fontWeight: "600", // Semi-bold
+    },
+
+    h4: {
+      fontSize: "22px",
+      fontWeight: "600", // Semi-bold
+    },
+
+    h5: {
+      fontSize: "16px",
+      fontWeight: "600", // Semi-bold
+    },
+
+    body1: {
+      fontSize: "16px",
+    },
+
+    body2: {
+      fontSize: "14px",
     },
   },
-}); 
+  palette: {
+    primary: {
+      main: "#F54749", // Primary color
+    },
+    secondary: {
+      main: "#03A9F4", // Secondary color
+    },
+    
+    background:{
+     default: "rgb(245, 245, 245)"
+    }
+  },
+});
 
 export default theme;
